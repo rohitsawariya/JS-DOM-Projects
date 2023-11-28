f9bb0ba2550457d5c5d4faa11202b33097eeec9b
rohitsawariya <rohitsawariya0@gmail.com>
2023-11-28 16:34:13 +0530

    Added Files Systematically

---
+++ BMI Calculator/script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        alert("Enter a valid height");
+        return;
+    }
+    if (weight === '' || weight < 0 || isNaN(weight)) {
+        alert("Enter a valid weight");
+        return;
+    }
+    else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        results.innerHTML = ` <span>
+            ${bmi}</span> `
+    }
+})
